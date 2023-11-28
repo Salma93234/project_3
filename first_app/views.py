b3260cbef2687d8939f03ceeb44d084b4e0ca09d
Salma.Akther <salma.akther@gmail.comm>
2023-11-28 22:19:53 +0600

    second-commit

--- first_app/views.py
+++ first_app/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 import datetime
 def home(request):
-    d={'author':'rahim','age':35,'list':[1,2,3],'lst':['phython','is','best'],'val':' ','birthday':datetime.datetime.now(),
+    d={'author':'rahim','age':35,'list':[1,2,3],'lst':['phython','is','best'],'val':' ',
+       
+       'value':['Apple', 'Mango', 'Orange'] ,
+       'my_data' : "cat\ndog\nhorse",
+        'value3':'MY NAME SALMA',
+        'value2':'you are wellcome in our programmin world',
+        'value4':['Banana', 'Mango', 'Orange'],
+         'some_list':['Father', 'Mother', 'Child'],
+        'my_data2' : "It's a new day",
+        
+         'people' : [
+        {'name': 'amy', 'age': 21},
+        {'name': 'joe', 'age': 31},
+        {'name': 'zed', 'age': 19},
+    ],
+
+
+        'value5':'Earthly',
+       'birthday':datetime.datetime.now(),
        'courses':[
         {
             'id':1,
